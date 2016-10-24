02fe603834ab7363aa6272e4e80ec1c1a4b55519
TomVanBoghout <tvboghout@gmail.com>
2016-10-24 15:05:07 +0200

    Check your commutes from last 10 rides

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 var strava = require('strava-v3');
-strava.athlete.get({},function(err,payload) {
+/*strava.athlete.get({},function(err,payload) {
     if(!err) {
         console.log(payload);
+    }
+    else {
+        console.log(err);
+    }
+});
+*/
+strava.athlete.listActivities({per_page:10},function(err,payload) {
+    if(!err) {
+        
+        for (var i = 0; i < payload.length; i++) {
+            if (payload[i].commute) {
+                console.log(payload[i].start_date_local + " " + payload[i].name + "");
+            }
+        }
     }
     else {
         console.log(err);
